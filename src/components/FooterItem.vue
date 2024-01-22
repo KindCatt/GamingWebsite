6dfc45c7e54f149634fb1b5935d008b25c55bbb2
KindCatt <ff159753f@yandex.ru>
2024-01-23 00:01:39 +0300

    Добавил разметку Footer, подключил компонент FooterItem

---
+++ src/components/FooterItem.vue
@@ -0,0 +1,21 @@
+<template>
+	<div class="footer-item">
+
+		<h5 class="footer-item__header logo">
+			<slot name="title"></slot>
+		</h5>
+
+		<p class="footer-item__content">
+			<slot name="info"></slot>
+		</p>
+
+		<div class="footer-item__footer">
+			<slot></slot>
+		</div>
+
+	</div>
+</template>
+
+<style lang="sass" scoped>
+
+</style>
--- src/components/Layout/FooterView.vue
+++ src/components/Layout/FooterView.vue
@@ -1,9 +1,63 @@
+<script setup>
+import FooterItem from "@/components/FooterItem.vue";
+</script>
+
 <template>
 	<footer class="footer-view">
-		<div style="font-size: 30px;">Footer</div>
+		
+		<div class="footer-view__top">
+			<div class="footer-view__row">
+
+				<div class="footer-view__column">
+					<FooterItem>
+						<template #title>LOGO</template>
+						<template #info>
+							Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+						</template>
+						@Logo
+					</FooterItem>
+				</div>
+
+				<div class="footer-view__column">
+					<FooterItem>
+						<template #title>About us</template>
+						<template #info>
+							<p><a class="footer-view__link" href="#">Zeux</a></p>
+							<p><a class="footer-view__link" href="#">Portfolio</a></p>
+							<p><a class="footer-view__link" href="#">Careers</a></p>
+							<p><a class="footer-view__link" href="#">Contact us</a></p>
+						</template>
+					</FooterItem>
+				</div>
+
+				<div class="footer-view__column">
+					<FooterItem>
+						<template #title>Contact us</template>
+						<template #info>
+							Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+						</template>
+						+908 89097 890
+					</FooterItem>
+				</div>
+
+				<div class="footer-view__column">
+					<FooterItem>
+						Здесь SVG иконки
+					</FooterItem>
+					
+				</div>
+			</div>
+
+			<div class="footer-view__bottom">
+				<p class="footer-view__bottom-text">Copyright ® 2022 prodesigner All rights Rcerved</p>
+			</div>
+		</div>
+
 	</footer>
 </template>
 
 <style>
-
+.footer-view__column {
+	border: 1px solid #000;
+}
 </style>
