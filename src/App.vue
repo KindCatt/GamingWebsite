54f402c40f4776206e71b603af880dd4efc7db16
KindCatt <ff159753f@yandex.ru>
2024-01-22 16:03:10 +0300

    Create project

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script>
+
+export default {
+	name: 'App',
+	components: {}
+}
+</script>
+
+<template>
+	<div class="app">
+
+		<RouterView />
+	</div>
+</template>
+
+<style lang="scss">
+
+</style>
